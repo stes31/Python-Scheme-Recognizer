90a48040d07dd49df1e7402acb179cfebbb3164a
Kirusha-DA <Kirill.erchov@mail.ru>
2023-05-12 16:36:20 +0300

    updated logic

--- text_detection.py
+++ text_detection.py
@@ -7,17 +7,22 @@ class Image:
         self.image = cv2.imread(path)
     
 
-    def read_text(self, *, language='ru'):
-        reader = easyocr.Reader([language], gpu=False)
-        text = reader.readtext(self.image)
+    def show_text(self, *, text):
         for t in text:
             bbox, text, score = t
             cv2.rectangle(self.image, [int(bbox[0][0]), int(bbox[0][1])], [int(bbox[2][0]), int(bbox[2][1])], (0, 255, 0), 5)
             cv2.putText(self.image, text, [int(bbox[0][0]), int(bbox[0][1])], cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
         plt.imshow(cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB))
         plt.show()
+    
+
+    def get_text(self, *, language='ru'):
+        reader = easyocr.Reader([language], gpu=False)
+        return reader.readtext(self.image)
+
 
 if __name__ == '__main__':
-    image_path = 'pictures/doc.jpg'
-    image = Image(path=image_path)
-    image.read_text(language='ru')
+    path = 'pictures/doc.jpg'
+    image = Image(path=path)
+    text = image.get_text(language='ru')
+    image.show_text(text=text)
