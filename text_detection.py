c4b273a1676712000aee57fe6fac04f884d3b723
BIVA <bulatnikov.i@mail.ru>
2023-05-18 01:11:28 +0300

    1

--- text_detection.py
+++ text_detection.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 class Image:
     def __init__(self, *, path):
         self.image = cv2.imread(path)
-    
+
 
     def show_text(self, *, text):
         for t in text:
@@ -14,10 +14,10 @@ class Image:
             cv2.putText(self.image, text, [int(bbox[0][0]), int(bbox[0][1])], cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
         plt.imshow(cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB))
         plt.show()
-    
+
 
     def get_text(self, *, language='ru'):
-        reader = easyocr.Reader([language], gpu=False)
+        reader = easyocr.Reader([language], gpu=True)
         return reader.readtext(self.image)
 
 
